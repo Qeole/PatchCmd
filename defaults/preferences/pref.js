a7904ad53bc8d6a3abe387fc217ad341c7856a6a
Qeole <qeole@outlook.com>
2016-05-05 17:46:29 +0200

    pref.js: Rename the file and set more sensible default preferences.
    Now that we can select the preferences through the user interface, we
    can safely disable the use of certain fields by default.
    
    * The most interesting fields are `--to`, `--cc` and `--message-id`
    
    * On the other hand, `--subject` or ``--from` are often obtained from
      git configuration or commit logs, so there should be disabled by
      default.
    
    * The prefix is not mandatory, but let's keep it. Anyway, it is easy to
      reselect the generated string without taking it, so it should not be a
      problem if it is not needed.
    
    * The current suffix, `./*.patch`, is just an example use of this
      option. We let the value but disable the field by default.
    
    * `--bcc` should be rarely needed but most often empty, it seems safe to
      keep it enabled.

--- defaults/preferences/pref.js
+++ defaults/preferences/pref.js
@@ -5,16 +5,16 @@ pref("patchCmd.optionName.to",         "--to=");
 pref("patchCmd.optionName.cc",         "--cc=");
 pref("patchCmd.optionName.bcc",        "--bcc=");
 pref("patchCmd.optionName.message-id", "--in-reply-to=");
-pref("patchCmd.optionName.suffix",     "");
+pref("patchCmd.optionName.suffix",     "./*.patch");
 
 pref("patchCmd.useField.prefix",     true);
-pref("patchCmd.useField.subject",    true);
-pref("patchCmd.useField.from",       true);
+pref("patchCmd.useField.subject",    false);
+pref("patchCmd.useField.from",       false);
 pref("patchCmd.useField.to",         true);
 pref("patchCmd.useField.cc",         true);
 pref("patchCmd.useField.bcc",        true);
 pref("patchCmd.useField.message-id", true);
-pref("patchCmd.useField.suffix",     true);
+pref("patchCmd.useField.suffix",     false);
 
 pref("patchCmd.regexp.pattern",    "");
 pref("patchCmd.regexp.flags",      "");
